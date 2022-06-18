2984a207a21402b4ee045519c9ee8d8899a95276
Vitaliy Sulimenko <prevetal@gmail.com>
2022-06-18 12:02:40 +0300

    +

--- js/blog.js
+++ js/blog.js
@@ -1,10 +1,14 @@
+const blog = document.getElementById('blog'),
+    article = document.getElementById('article'),
+    ipfsStatus = document.querySelector('header .ipfs_status')
+
+
 class Tweets {
     neuron = 'bostrom1ndwqfv2skglrmsqu4wlneepthyrquf9r7sx6r0'
     particleFrom = 'QmbdH2WBamyKLPE5zu4mJ9v49qvY8BFfoumoVPMR5V4Rvx'
     limit = 1000000000
     loadURL = `https://lcd.bostrom.cybernode.ai/txs?cyberlink.neuron=${this.neuron}&cyberlink.particleFrom=${this.particleFrom}&limit=${this.limit}`
     loadData = []
-    // tweets = []
     node = {}
     html = ''
 
@@ -42,8 +46,6 @@ class Tweets {
 
 
         if (this.node !== null) {
-            const ipfsStatus = document.querySelector('header .ipfs_status')
-
             if (ipfsStatus) { ipfsStatus.classList.add('green') }
         } else {
             // запосной вариант загрузки
@@ -55,11 +57,12 @@ class Tweets {
             .then(data => this.loadData = data.txs.sort((a, b) => Date.parse(b.timestamp) - Date.parse(a.timestamp)))
     }
 
-    async parseTweets() {
+
+    async parseALLTweets() {
         await this.loadTweets()
 
         if (this.node !== null) {
-            let tweetsEl = document.getElementById('blog').querySelector('.row')
+            let tweetsEl = blog.querySelector('.row')
 
             for (const el of this.loadData) {
                 let cid = el.tx.value.msg[0].value.links[0].to,
@@ -72,38 +75,97 @@ class Tweets {
                 for await (const message of await all(this.node.cat(cid))) {
                     let item = new TextDecoder().decode(message)
 
-                    // this.tweets.push({
-                    //     date: time,
-                    //     text: item
-                    // })
-
                     if (tweetsEl) {
                         tweetsEl.innerHTML += String()
                             + '<div class="item">'
                             + '<div class="date">' + time + '</div>'
-                            + '<a href="./" class="title"></a>'
+                            + '<a href="./blog_item.html?cid=' + cid + '" class="title"></a>'
                             + '<div class="desc text_block">' + marked.parse(item) + '</div>'
                             + '</div>'
                     }
                 }
-
-                // Переносим заголовок
-                let articles = document.querySelectorAll('.blog .item')
-
-                articles.forEach(element => {
-                    let firstTitle = element.querySelector('.desc h2:first-child')
-
-                    if (firstTitle) {
-                        element.querySelector('.title').textContent = firstTitle.textContent
-
-                        firstTitle.remove()
-                    }
-                })
-
-                // Убираем лоадер
-                const loader = document.querySelector('.blog .loader')
-                if (loader) { loader.remove() }
             }
+
+
+            // Переносим заголовок
+            let articles = blog.querySelectorAll('.item')
+
+            articles.forEach(element => {
+                let firstTitle = element.querySelector('.desc h2:first-child')
+
+                if (firstTitle) {
+                    element.querySelector('.title').textContent = firstTitle.textContent
+
+                    firstTitle.remove()
+                }
+            })
+
+
+            // Добавление аттрибутов ссылкам
+            let links = blog.querySelectorAll('.item .desc a')
+
+            links.forEach(element => {
+                element.setAttribute('target', '_blank')
+                element.setAttribute('rel', 'noopener nofollow')
+            })
+
+
+            // Убираем лоадер
+            const loader = blog.querySelector('.loader')
+            if (loader) { loader.remove() }
+        }
+    }
+
+
+    async parseOnTweet() {
+        await this.loadTweets()
+
+        if (this.node !== null) {
+            let urlCID = new URL(window.location.href).searchParams.get('cid')
+
+            // Определение даты
+            for (const el of this.loadData) {
+                if (urlCID == el.tx.value.msg[0].value.links[0].to) {
+                    let time = new Date(el.timestamp).toLocaleDateString('en-US', {
+                        year: 'numeric',
+                        month: 'long',
+                        day: 'numeric'
+                    })
+
+                    article.querySelector('.date').innerText = time
+                }
+            }
+
+            // Получение контента поста
+            for await (const message of await all(this.node.cat(urlCID))) {
+                let item = new TextDecoder().decode(message)
+
+                article.querySelector('.text_block').innerHTML = marked.parse(item)
+            }
+
+
+            // Переносим заголовок
+            let firstTitle = article.querySelector('.text_block h2:first-child')
+
+            if (firstTitle) {
+                article.querySelector('.title').textContent = firstTitle.textContent
+
+                firstTitle.remove()
+            }
+
+
+            // Добавление аттрибутов ссылкам
+            let links = article.querySelectorAll('.text_block a')
+
+            links.forEach(element => {
+                element.setAttribute('target', '_blank')
+                element.setAttribute('rel', 'noopener nofollow')
+            })
+
+
+            // Убираем лоадер
+            const loader = article.querySelector('.loader')
+            if (loader) { loader.remove() }
         }
     }
 }
@@ -124,4 +186,5 @@ const all = async source => {
 
 let tweets = new Tweets()
 
-tweets.parseTweets()
+if (blog) { tweets.parseALLTweets() }
+if (article) { tweets.parseOnTweet() }
